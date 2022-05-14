61445f3a3c3f031f2dabe18ed539e99661e25cb1
Ubiquitous Data & Intelligent Computing Lab <yfan@dragon.nchu.edu.tw>
2022-05-14 17:29:02 +0800

    debug

--- DocumentProcessor.py
+++ DocumentProcessor.py
@@ -7,14 +7,14 @@ def DocumentUnitProcessor(inputfile, sentence_level=True):
     if sentence_level==True:
         id_counter=0
         for id in inputfile:
-            if input[id]["全文"]!= None:
-                for sentence in input[id]["全文"].split("。"):
+            if inputfile[id]["全文"]!= None:
+                for sentence in inputfile[id]["全文"].split("。"):
                     if len(sentence) < 512:                        #(移掉超過512長度的句子)
                         id_counter = id_counter + 1  
                         pyserini_format_json_list.append({"id":id_counter, "contents":sentence})
     else:
-        for id in input:
-            if input[id]["全文"]!= None:
+        for id in inputfile:
+            if inputfile[id]["全文"]!= None:
                 pyserini_format_json_list.append({"id":id, "contents":input[id]["全文"]})# 使用內文為單位進行index construction
 
     return pyserini_format_json_list
